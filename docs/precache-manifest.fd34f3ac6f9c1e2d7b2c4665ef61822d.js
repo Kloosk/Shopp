c30df4fa27328f32c27f0c6c73998c615c00ccf4
Lukasz <lukasz.gras.97@gmail.com>
2020-09-25 11:19:23 +0200

    Update docs

---
+++ docs/precache-manifest.fd34f3ac6f9c1e2d7b2c4665ef61822d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62cb17a6d259a9bc9b9f6da771382dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c96a0994d9509a71fcf",
+    "url": "./static/css/2.cf9f72d1.chunk.css"
+  },
+  {
+    "revision": "5c96a0994d9509a71fcf",
+    "url": "./static/js/2.e0014c86.chunk.js"
+  },
+  {
+    "revision": "4e97246dc4cdcfd8b3a8430a358b61e5",
+    "url": "./static/js/2.e0014c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105e737dced76d0a2388",
+    "url": "./static/js/main.0a06cf17.chunk.js"
+  },
+  {
+    "revision": "3276f104cf8605a9d966",
+    "url": "./static/js/runtime-main.6e119779.js"
+  },
+  {
+    "revision": "34cd3cac1cf0a646700cce64cc51ae2a",
+    "url": "./static/media/adidas.34cd3cac.png"
+  },
+  {
+    "revision": "1bfd3bbbab98cbdb616dc2b69301f1c5",
+    "url": "./static/media/img.1bfd3bbb.jpg"
+  },
+  {
+    "revision": "b1f3ad934d9cd089a4b5ccbbe858f554",
+    "url": "./static/media/img.b1f3ad93.jpg"
+  },
+  {
+    "revision": "dbcd7efdbd2c76a77e17966edd648f6f",
+    "url": "./static/media/img1.dbcd7efd.jpg"
+  },
+  {
+    "revision": "d90e3b5ca68ea4b359965bd52a243be1",
+    "url": "./static/media/img2.d90e3b5c.jpg"
+  },
+  {
+    "revision": "7f73721706ee642fe57befdb26214a6e",
+    "url": "./static/media/imgg.7f737217.jpg"
+  },
+  {
+    "revision": "7ddd1f26cb3fbf34d72f621768ff6322",
+    "url": "./static/media/new.7ddd1f26.png"
+  },
+  {
+    "revision": "1323439fa9dfe25aa49addcf5e078e49",
+    "url": "./static/media/nike.1323439f.png"
+  },
+  {
+    "revision": "7c14f5b86e42b8e0ae0d91dd2bfc2dfe",
+    "url": "./static/media/reebok.7c14f5b8.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/components/cart/Step.js
+++ src/components/cart/Step.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components'
-import Logo from "../nav/logo/Logo";
 
 const Container = styled.div`
   width: 100%;
